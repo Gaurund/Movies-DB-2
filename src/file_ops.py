0bd1360d612220c493fab35fc1207b88a8b07aeb
Gaurund <pavel.makushev@gmail.com>
2025-10-10 15:31:52 +0300

    Add descriptions

--- src/file_ops.py
+++ src/file_ops.py
@@ -8,6 +8,19 @@ from os.path import getsize
 
 @dataclass
 class RealFile:
+    """
+    Store
+
+    `name`: str
+
+    `path`: str
+
+    `size`: int
+
+    `last_mod`: datetime
+
+    `stat_ino`: str
+    """
     name: str
     path: str
     size: int
@@ -17,55 +30,40 @@ class RealFile:
 
 @dataclass
 class DeviceFiles:
+    """
+    Store list of files and the unique ID of the device.
+
+    `stat_dev`: str
+    
+    `files`: list[RealFile]
+    """
     stat_dev: str
     files: list[RealFile]
 
 
-class StoreFile:
-    def __init__(self, root, file) -> None:
-        self.root = root
-        self.file = file
-
-    def store(self) -> RealFile:
-        full_path: Path = Path(self.root, self.file)
-        return RealFile(
-            name=self.file,
-            path=str(self.root),
-            size=getsize(full_path),
-            last_mod=datetime.fromtimestamp(full_path.stat().st_mtime),
-            stat_ino=str(full_path.stat().st_ino),
-        )
-
-
-class StoreDevFiles:
-    def __init__(self, path, walked) -> None:
-        self.path = path
-        self.walked = walked
-
-    def store(self) -> DeviceFiles:
-        disk_files = DeviceFiles(
-            stat_dev=str(Path(self.path).stat().st_dev), files=list()
-        )
-        for root, dirs, files in self.walked:
-            for file in files:
-                if (
-                    "$RECYCLE.BIN" not in str(root)
-                    and not mimetypes.guess_file_type(file)[0] == None
-                    and mimetypes.guess_file_type(file)[0].startswith("video")
-                ):
-                    store_file = StoreFile(root=root, file=file)
-                    disk_files.files.append(store_file.store())
-        return disk_files
-
-
-class ScanDev:
-    def __init__(self, init_path: str) -> None:
-        self.path: Path = Path(init_path)
-
-    def walk_dirs(self) -> DeviceFiles:
-        walked: Iterator[tuple[Path, list[str], list[str]]] = Path.walk(self.path)
-        store_dev_files = StoreDevFiles(path=self.path, walked=walked)
-        disk_files = store_dev_files.store()
-        return disk_files
-    
-    
+def get_dev_files(path_str) -> DeviceFiles:
+    """
+    Take a string with Windows' path and return the `DeviceFiles` object
+    with all video files in it.
+    """
+    path: Path = Path(path_str)
+    walked: Iterator[tuple[Path, list[str], list[str]]] = Path.walk(path)
+    dev_files = DeviceFiles(stat_dev=str(Path(path).stat().st_dev), files=list())
+    for root, dirs, files in walked:
+        for file in files:
+            if (
+                "$RECYCLE.BIN" not in str(root)
+                and not mimetypes.guess_file_type(file)[0] == None
+                and mimetypes.guess_file_type(file)[0].startswith("video")
+            ):
+                full_path: Path = Path(root, file)
+                dev_files.files.append(
+                    RealFile(
+                        name=file,
+                        path=str(root),
+                        size=getsize(full_path),
+                        last_mod=datetime.fromtimestamp(full_path.stat().st_mtime),
+                        stat_ino=str(full_path.stat().st_ino),
+                    )
+                )
+    return dev_files
